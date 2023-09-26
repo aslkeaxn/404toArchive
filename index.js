dd39f71529e83d57732a6b3b3ad4a2b0016776e5
aslkeaxn <69468068+aslkeaxn@users.noreply.github.com>
2023-09-26 13:14:56 +0300

    script

---
+++ index.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         404toWebArchive
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  async function main() {
+    const url = location.href;
+    const res1 = await fetch(url, { method: "HEAD" });
+
+    if (res1.status !== 404) return;
+
+    const encodedUrl = encodeURI(url);
+    const archiveUrl = `https://archive.org/wayback/available?url=${encodedUrl}`;
+    const res2 = await fetch(archiveUrl);
+    const json = await res2.json();
+    const closestSnapshot = json.archived_snapshots.closest;
+
+    if (!closestSnapshot || !closestSnapshot.available) return;
+
+    window.location.replace(closestSnapshot.url);
+  }
+
+  main().catch(console.error);
+})();
